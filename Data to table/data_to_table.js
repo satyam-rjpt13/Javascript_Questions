fb710b96ddc42517b57d5ba3f4d88b538d2f302e
satyam-rjpt13 <satyam974y@gmail.com>
2025-11-27 00:08:35 +0530

    Data to Table

---
+++ Data to table/data_to_table.js
@@ -0,0 +1,16 @@
+let name = document.getElementById('name');
+let ageInpt = document.getElementById('age');
+let countryName = document.getElementById('country');
+const addRow = document.getElementById('addRow');
+const search = document.getElementById('search');
+const tbody = document.getElementById('table body');
+
+addRow.addEventListener('click',()=>{
+    let tr = createElement('tr');
+
+
+})
+
+search.addEventListener('click',()=>{
+
+})
