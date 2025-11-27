b31c47a8ae339690ecd07e6cf9fc9318f6114011
satyam-rjpt13 <satyam974y@gmail.com>
2025-11-27 22:57:02 +0530

    js file updated code

--- Data to table/data_to_table.js
+++ Data to table/data_to_table.js
@@ -1,16 +1,49 @@
-let name = document.getElementById('name');
-let ageInpt = document.getElementById('age');
-let countryName = document.getElementById('country');
 const addRow = document.getElementById('addRow');
-const search = document.getElementById('search');
-const tbody = document.getElementById('table body');
+const searchBtn = document.getElementById('search');
+const tbody = document.querySelector('table tbody');
 
 addRow.addEventListener('click',()=>{
-    let tr = createElement('tr');
 
+    
 
+    // fatching name/ getting inputs
+    let name = document.getElementById('name').value;
+    let ageInpt = document.getElementById('age').value;
+    let countryName = document.getElementById('country').value;
+
+    if(name ==="" || age === "" || countryName === ""){
+        alert("Please fill all the details");
+        return;
+    }
+
+    // new row created
+    let tr = document.createElement('tr');
+
+    // creating table data
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+
+    // adding data in column
+    td1.innerHTML = name;
+    td2.innerHTML =ageInpt;
+    td3.innerHTML = countryName;
+
+    // adding column in row
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    // adding row in body
+    tbody.appendChild(tr);
+
+    // clearing inputs
+    document.getElementById('name').value ="";
+    document.getElementById('age').value ="";
+    document.getElementById('country').value ="";
+    
 })
 
-search.addEventListener('click',()=>{
+searchBtn.addEventListener('click',()=>{
 
 })
